96307ff4397c3194a14f373c6b4bdf5ccf745f3c
曹磊 <caolei01@baijiahulian.com>
2021-04-12 23:37:24 +0800

    gl渲染环境，三种缓冲区 颜色缓冲主要，深度和Stencil辅助 glFlush 把cpu指令发给gpu

---
+++ MAC/OGLDataBuffOSX/OGLWindowOSX/scene.cpp
@@ -0,0 +1,8 @@
+//
+//  scene.cpp
+//  OGLWindowOSX
+//
+//  Created by 曹磊 on 2021/4/12.
+//
+
+#include "scene.hpp"
---
+++ MAC/OGLDataBuffOSX/OGLWindowOSX/scene.h
@@ -0,0 +1,17 @@
+//
+//  scene.hpp
+//  OGLWindowOSX
+//
+//  Created by 曹磊 on 2021/4/12.
+//
+
+#ifndef scene_hpp
+#define scene_hpp
+
+#include <stdio.h>
+
+void Init(float width, float height);
+void OnViewportChanged(float width, float heigth);
+void Render();
+
+#endif /* scene_hpp */
