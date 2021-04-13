6ed0feca207db2ba7362f73719e7873d8180a506
曹磊 <caolei01@baijiahulian.com>
2021-04-13 23:38:33 +0800

    feat: 矩阵压栈、出栈

--- MAC/06OGLMatrix_ stackOSX/OGLWindowOSX/scene.cpp
+++ MAC/06OGLMatrix_ stackOSX/OGLWindowOSX/scene.cpp
@@ -31,12 +31,13 @@ void Init(float width, float height) {
 void Render() {
     //双缓冲 0缓冲 1缓冲; GPU把数据绘制到缓冲区里面
     //画完，会把数据0号缓冲区刷到屏幕上，接着1号缓冲区  连续不断的刷
-    
     //实际上，双缓冲 没有1号的 GPU GPU在后台绘制一个窗口 然后在绘制到屏幕上面
     //d单缓冲 GPU直接绘制屏幕上
-    
     //深度缓冲区、Stencil缓冲区 辅助绘制的
     //颜色缓冲区主要绘制
+    //显卡细节; 调用 glClearColor、glClear 不一定在GPU执行指令
+    //这些指令CPU端，opengl驱动里面 都在 driver里面，有一个 commad queue
+    //glFlush 把CPU端，opengl驱动里面的指令 发给 GPU; 不知道是否执行完没有
     
     //清除颜色缓冲区使用的颜色
     glClearColor(0.1f, 0.4f, 0.7f, 1.0f);
@@ -52,6 +53,9 @@ void Render() {
     //glPolygonMode(GL_FRONT,GL_LINE);
     //重置下矩阵
     glLoadIdentity();
+    
+    //glPushMatrix glPopMatrix 记录当前的矩阵，pop出之前的矩阵状态
+    glPushMatrix();
     glTranslatef(0.0f, 0.0f, -2.0f);
     glRotatef(-45.0f, 0.0f, 1.0f, 0.0f);
     glScalef(0.5f, 0.6f, 0.7f);
@@ -60,6 +64,16 @@ void Render() {
     glVertex3f(-0.5f, -0.5f, 0.0f);
     glVertex3f(-0.0f, -0.5f, 0.0f);
     glEnd();
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, -2.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex3f(0.0f+0.5f, 0.0f, 0.0f);
+    glVertex3f(-0.5f+0.5f, -0.5f, 0.0f);
+    glVertex3f(-0.0f+0.5f, -0.5f, 0.0f);
+    glEnd();
+    glPopMatrix();
     
 //    //glLoadIdentity();
 //    //效果累加
@@ -74,7 +88,5 @@ void Render() {
     glFlush();
     
     
-    //显卡细节; 调用 glClearColor、glClear 不一定在GPU执行指令
-    //这些指令CPU端，opengl驱动里面 都在 driver里面，有一个 commad queue
-    //glFlush 把CPU端，opengl驱动里面的指令 发给 GPU; 不知道是否执行完没有
+
 }
